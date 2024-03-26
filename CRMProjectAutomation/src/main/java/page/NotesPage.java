bf8c451b4ec384a5ca3ffc42b5cdda72fa177774
safnaahamed <safna_ahamed@yahoo.co.in>
2024-03-26 21:55:21 +0530

    pushed

--- CRMProjectAutomation/src/main/java/page/NotesPage.java
+++ CRMProjectAutomation/src/main/java/page/NotesPage.java
@@ -1,15 +1,11 @@
 package page;
 
-import java.time.Duration;
 import java.util.List;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.FluentWait;
-
 import utility.ElementUtility;
 import utility.WaitUtility;
 
--- CRMProjectAutomation/src/main/java/utility/WaitUtility.java
+++ CRMProjectAutomation/src/main/java/utility/WaitUtility.java
@@ -4,11 +4,9 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitUtility {
